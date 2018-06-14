87738e84db1e12cea9f65c725bfae87e24759822
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-14 10:45:51 -0300

    Acabei o cadastro de empresas

--- src/components/CadastroEmpresas.jsx
+++ src/components/CadastroEmpresas.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react';
 import axios from 'axios';
 
 import { CadastroEmpresasTable, CadastroEmpresasForm } from '.';
+import { api } from '../services';
 
 class CadastroEmpresas extends Component {
   state = {
@@ -9,26 +10,49 @@ class CadastroEmpresas extends Component {
   }
 
   componentDidMount() {
+    const empresas = [];
 
+    axios
+      .get(`${api}/empresas`)
+      .then((res) => {
+        res.data.forEach(el => empresas.push({ ...el, key: el.cnpj }));
+        this.setState({ empresas });
+      })
+      .catch(err => console.error(err));
   }
 
-  adicionarEmpresaHandle = ({ nome, cnpj, numero }) => {
+  adicionarEmpresaHandle = (empresa) => {
     const empresas = [...this.state.empresas];
 
-    if (empresas.findIndex(el => el.cnpj === cnpj) === -1) {
+    const id = empresas.findIndex(el => el.cnpj === empresa.cnpj);
+
+    if (id === -1) {
       empresas.push({
-        nome,
-        cnpj,
-        numero,
-        key: cnpj,
+        ...empresa,
+        key: empresa.cnpj,
       });
+    } else {
+      empresas[id] = {
+        ...empresa,
+        key: empresa.cnpj,
+      };
     }
-    this.setState({ empresas });
+
+    axios
+      .put(`${api}/empresas/${empresa.numero}`, { empresa })
+      .then(() => this.setState({ empresas }))
+      .catch(err => console.error(err));
   }
 
   deletarEmpresaHandle = (key) => {
     const empresas = [...this.state.empresas];
-    this.setState({ empresas: empresas.filter(item => item.key !== key) });
+
+    const empresa = empresas.find(item => item.key === key);
+
+    axios
+      .delete(`${api}/empresas/${empresa.numero}`)
+      .then(() => this.setState({ empresas: empresas.filter(item => item.key !== key) }))
+      .catch(err => console.error(err));
   }
 
   render() {
