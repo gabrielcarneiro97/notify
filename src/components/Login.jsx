7c1c1cd36bc0b5b982b236297b39b26d6fb357d7
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-18 10:30:22 -0300

    Refatorei Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { Row, Col, Layout } from 'antd';
 import PropTypes from 'prop-types';
